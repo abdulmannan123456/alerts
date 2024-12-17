460016ba13e564f116a06f42d4bba0a5fa06b351
abdulmannan123456 <abdul4309999@gmail.com>
2024-12-17 22:19:12 +0500

    initialize project

---
+++ app.js
@@ -0,0 +1,15 @@
+//chapter 1
+//question 1
+alert("Hellow");
+//qestion 2
+alert("MY name is abdul mannan");
+//qestion 3
+alert("Error! Please enter a valid password.");
+//qestion 4
+alert("Welcome to JS Land... \nHappy Coding!"); 
+//qestion 5
+alert("Welcome to JS Land..."); 
+//qestion 6
+alert("Happy Coding!");  
+//question 7
+alert("Hellow... i can run JS through my web browser's console");                                                        
